614cd292305b6681d218de1be80baa35e8bc4cfe
Ben Nativi <bmn8@duke.edu>
2017-10-28 21:28:03 -0400

    First push with letter.service.ts

---
+++ src/app/services/letter.service.ts
@@ -0,0 +1,36 @@
+import { Letter } from '../models/letter';
+
+import { Injectable }    from '@angular/core';
+import { Headers, Http } from '@angular/http';
+ 
+import 'rxjs/add/operator/toPromise';
+ 
+@Injectable()
+export class LetterService {
+ 
+    private headers = new Headers({'Content-Type': 'application/json'});
+
+    private letterUrl = 'http://localhost:3000/create';  // Url that all info Letter 's will be stored at
+
+    constructor(private http: Http) { }
+
+    getLetter(): Promise<Letter> {  //pulls off a Letter class object letter from a storage at a url
+        return this.http.get(this.letterUrl)
+                .toPromise()    //makes the server wait until information is returned
+                .then(response => response.json().data as Letter)
+                .catch(this.handleError);   //catches an error if no letter class object exists at that url
+    }
+
+    update(letter: Letter): Promise<Letter> {   //puts a Letter class object letter at the url letterUrl/letter.id
+        const url = `${this.letterUrl}/${letter.id}`;   //generates the storage url for letter
+        return this.http.put(url, JSON.stringify(letter), {headers: this.headers}) 
+          .toPromise()      //makes the server wait until information is returned
+          .then(() => letter)   //???
+          .catch(this.handleError);     //catches an error if no letter class object exists
+    }
+
+    private handleError(error: any): Promise<any> {     //error message if any error occurs
+        console.error('An error occurred', error); 
+        return Promise.reject(error.message || error);
+    }
+}
