dd5a248b41f0df08e48b4585345a6850540b0367
Ethan Holland <ethanjholland@gmail.com>
2018-02-15 12:19:04 -0500

    updated public dns for correct ec2 instance

---
+++ src/app/constants.ts
@@ -0,0 +1,13 @@
+//note this file is git ignored
+
+//dev
+// export class Constants {
+//     public static URL='http://localhost:4200';
+//     public static API_URL='http://localhost:3000'
+// }
+
+deploy
+export class Constants {
+    public static URL='http://theletterdesk.com';
+    public static API_URL='ec2-54-208-75-189.compute-1.amazonaws.com:3000' //note this changes every time the ec2 instance is stopped and restarted
+}
