c45ed839bd245f93ee8b5815d15f93e9b2f9041f
Ethan Holland <ethanjholland@gmail.com>
2020-06-02 21:55:58 -0400

    only log in production mode

--- src/app/services/google-analytics.service.ts
+++ src/app/services/google-analytics.service.ts
@@ -1,3 +1,4 @@
+import { environment } from '../../environments/environment';
 import { Injectable } from '@angular/core';
 import { PrivateConstants } from '../constants.private';
 
@@ -6,11 +7,15 @@ declare let gtag; //gtag comes from google analytics script in head of index.htm
 @Injectable()
 export class GoogleAnalyticsService {
     public logPage(pageName: string) {
-        gtag('config', PrivateConstants.GOOGLE_ANALYTICS_ID, {page_title: pageName, page_path: '/' + pageName});
-        this.logEvent('pageview', pageName); //additionally log an event under the pageview category
+        if (environment.production) { // only log pages in production mode
+            gtag('config', PrivateConstants.GOOGLE_ANALYTICS_ID, {page_title: pageName, page_path: '/' + pageName});
+            this.logEvent('pageview', pageName); //additionally log an event under the pageview category
+        }
     }
 
     public logEvent(category: string, eventName: string) {
-        gtag('event', eventName, {event_category: category})
+        if (environment.production) { // only log events in production mode
+            gtag('event', eventName, {event_category: category});
+        }
     }
 }
